d9ebee39585aed37b6d8b5ee77f31468735debfc
Joe D'Alessandro <joe@thejoeshow.net>
2014-08-07 12:49:23 -0400

    added no width space characters to the end of node insertion to ensure proper cursor placement.

--- dist/js/jquery.atwho.js
+++ dist/js/jquery.atwho.js
@@ -1,4 +1,4 @@
-/*! jquery.atwho - v0.4.11 - 2014-08-06
+/*! jquery.atwho - v0.4.11 - 2014-08-07
 * Copyright (c) 2014 chord.luo <chord.luo@gmail.com>; 
 * homepage: http://ichord.github.com/At.js 
 * Licensed MIT
@@ -349,7 +349,7 @@ Controller = (function() {
     if ($inputor.attr('contentEditable') === 'true') {
       class_name = "atwho-view-flag atwho-view-flag-" + (this.get_opt('alias') || this.at);
       content_node = "" + content;
-      insert_node = "<span contenteditable='false' class='" + class_name + "'>" + content_node + "</span>";
+      insert_node = "<span contenteditable='false' class='" + class_name + "'>" + content_node + "</span><span class='zero-width-space'>​</span>";
       $insert_node = $(insert_node, this.app.document).data('atwho-data-item', $li.data('item-data'));
       if (this.app.document.selection) {
         $insert_node = $("<span contenteditable='true'></span>", this.app.document).html($insert_node);
--- src/controller.coffee
+++ src/controller.coffee
@@ -128,7 +128,7 @@ class Controller
     if $inputor.attr('contentEditable') == 'true'
       class_name = "atwho-view-flag atwho-view-flag-#{this.get_opt('alias') || @at}"
       content_node = "#{content}"
-      insert_node = "<span contenteditable='false' class='#{class_name}'>#{content_node}</span>"
+      insert_node = "<span contenteditable='false' class='#{class_name}'>#{content_node}</span><span class='zero-width-space'>​</span>"
       $insert_node = $(insert_node, @app.document).data('atwho-data-item', $li.data('item-data'))
       if @app.document.selection
         $insert_node = $("<span contenteditable='true'></span>", @app.document).html($insert_node)
